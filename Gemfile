8c313c6a176a7995ced88b1e1c06d30119c8b2c3
kspurgin <kspurgin@users.noreply.github.com>
2024-10-11 17:41:47 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem "kiba-extend", "~> 4.0.1", github: "lyrasis/kiba-extend", branch: "main"
+
+group :development, :test do
+  gem "pry"
+  gem "rake"
+  gem "rspec"
+  gem "almost_standard", github: "kspurgin/almost_standard", branch: "main"
+end
---
+++ lib/ke_project/jobs/locations/clean.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module KeProject
+  module Jobs
+    module Locations
+      module Clean
+        module_function
+
+        def job
+          Kiba::Extend::Jobs::Job.new(
+            files: {
+              source: :orig__locations,
+              destination: :locations__clean,
+              lookup: %i[
+                type__location_types
+                locations__clean_rev
+              ]
+            },
+            transformer: xforms
+          )
+        end
+
+        def xforms
+          Kiba.job_segment do
+            transform Rename::Field, from: :loc_id, to: :location_id
+            transform KeProject::Transforms::Locations::LocNameReverser,
+              replace: true
+            transform Merge::MultiRowLookup,
+              lookup: type__location_types,
+              keycolumn: :loctypeid,
+              fieldmap: {location_type: :loctype}
+            transform Delete::Fields, fields: %i[updated_date loctypeid]
+            transform Merge::MultiRowLookup,
+              lookup: locations__clean_rev,
+              keycolumn: :location_id,
+              fieldmap: {unreversed_location: :loc_name}
+          end
+        end
+      end
+    end
+  end
+end
